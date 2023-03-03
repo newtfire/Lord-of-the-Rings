0928ae911de9dda97c07f9eaa4c870e0df6ad926
Stach13 <112652730+Stach13@users.noreply.github.com>
2023-03-03 13:14:58 -0500

    Added startup python file with venv and setting up other directories

--- txt/lotr-files.py
+++ txt/lotr-files.py
@@ -1,2 +1,13 @@
+import spacy
 import os
-import 
+
+nlp = spacy.load('en_core_web_md')
+
+workingDir = os.getcwd()
+print("source-txt" + workingDir)
+
+insideDir = os.listdir(workingDir)
+print('01-The_Fellowship-Of-The_Ring.txt', '02-The-Two-Towers.txt', '03-The-Return-Of-The-King.txt' + str(insideDir))
+
+CollPath = os.path.join(workingDir, 'source-txt')
+print(CollPath)
