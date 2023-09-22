a5b76ff2df4af49d1f61e107cfc1aa1462ae4f17
Stach13 <112652730+Stach13@users.noreply.github.com>
2023-09-22 14:59:50 -0400

    attempt at addEventListener('click') for text elements

--- docs/images/interact.js
+++ docs/images/interact.js
@@ -3,20 +3,26 @@ window.addEventListener('DOMContentLoaded', init, false);
 
 const nlp = ['ORG', 'GPE', 'NORP', 'LOC', 'FAC'];
 const x1 = '0'
+let bars = document.querySelectorAll('g[id]');
 // var x2 = getAttribute('x2')
 
-function init() { 
-    let bars = document.querySelectorAll('g[id]');
-    
-    for (let i="0"; i < nlp.length; i++) {
-        console.log(nlp[i])
-    }
+function init() {     
+    // for (let i="0"; i < nlp.length; i++) {
+        //console.log(nlp[i])
+  //  }
     
     for (let i = 0; i < bars.length; i++) {
     var line  = bars[i].querySelector('line')
     var x2 = line.getAttribute('x2')
-    console.log(x2)
-    alert(x2);
+    //console.log(x2)
+    //alert(x2);
     }
+    
+}
+
+function displayBars() {
+    for (let i = 0; i < bars.length; i++) {
+        document.bars[i].getElementsByClassName('text').addEventlistener('click', displayBars);
+        }
     
 }
