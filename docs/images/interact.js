53019afe1f97bd679de907007c578467f641b2a8
Stach13 <112652730+Stach13@users.noreply.github.com>
2023-09-27 16:01:52 -0400

    created a textButton() reveal of bars in svg graph

--- docs/images/interact.js
+++ docs/images/interact.js
@@ -1,28 +1,14 @@
 window.addEventListener('DOMContentLoaded', init, false);
 
-
-const nlp = ['ORG', 'GPE', 'NORP', 'LOC', 'FAC'];
-const x1 = '0'
-let bars = document.querySelectorAll('g[id]');
-// var x2 = getAttribute('x2')
-
-function init() {     
-    // for (let i="0"; i < nlp.length; i++) {
-        //console.log(nlp[i])
-  //  }
-    
-    for (let i = 0; i < bars.length; i++) {
-    var line  = bars[i].querySelector('line')
-    var x2 = line.getAttribute('x2')
-    //console.log(x2)
-    //alert(x2);
-    }
-    
+function init(){
+    var textButton = document.querySelector('.textButton')
+    textButton.addEventListener('click', hideShow, false)
 }
 
-function displayBars() {
-    for (let i = 0; i < bars.length; i++) {
-        document.bars[i].getElementsByClassName('text').addEventlistener('click', displayBars);
-        }
-    
+function hideShow(){
+    var bars = document.querySelector('.displayBars')
+    if (bars.style.display == 'none'){
+        bars.style.display='block';
+    }
+    else {bars.style.display='none'}
 }
