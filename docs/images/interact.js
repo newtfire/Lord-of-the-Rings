800b618177983cf43ec3ec5adbcd8a51a44f6282
Stach13 <112652730+Stach13@users.noreply.github.com>
2023-10-13 13:35:34 -0400

    Adding animate tag to svg and image map with anchor links

--- docs/images/interact.js
+++ docs/images/interact.js
@@ -5,27 +5,21 @@ const bars = document.getElementsByClassName('displayBars');
 
 function init(){
     for (i = 0; i < text.length; i++){
-        text[i].addEventListener('click', Show, false)
+        text[i].addEventListener('click', Show, false);        
     }
 }
 
 function Show(){
     Hide();
-    var x2 = document.bars.getAttribute('x2');
-    var width = 1;
-    var identity = setInterval(scene, 10);
-    function scene() {
-        for (i = 0; i < bars.length; i++){
-    	  if (width[i] == x2){
-    	      clearInterval(identity);
-    	} else {
-    	      width++;
-    	      bars.style.width = width + '%';
-    	  }
-    }
-  }
+    for (i = 0; i < bars.length; i++){
+        if (bars[i].id.split('-')[1] == this.id.split('-')[1]){
+            bars[i].document.getElementsByTagName('animate');
+            
+        } 
+        
+    } 
 }
-    
+
 function Hide(){
     for (i = 0; i < bars.length; i++){
         bars[i].style.display="none"; 
---
+++ docs/img-map.js
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded', init, false);
+
+const area = document.getElementsByTagName('area');
+
