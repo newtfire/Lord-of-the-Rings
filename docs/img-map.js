71dd21f32123a6cb3d8abcca64e6ec48a3cd344e
Stach13 <112652730+Stach13@users.noreply.github.com>
2023-11-10 15:06:48 -0500

    tried working on img-map.js

--- docs/img-map.js
+++ docs/img-map.js
@@ -1,18 +1,21 @@
 window.addEventListener('DOMContentLoaded', init, false);
 
 const table_row = document.getElementsByTagName('tr');
-const ellipse = document.getElementsByTagName('ellipse').getAttribute('id');
-const path = document.getElementsByTagName('path').getAttribute('id');
+const ellipse = document.querySelectorAll("#Dunland, #Weathertop, #Bree, #Aglarond, #Dale, #Dwarves-Erebor, #Elf-Rivendell, #Orc-Gundabad, #Orc-Isengard, #Elf-Woodland, #Elf-Mithlond");
+const path = document.querySelectorAll("#Mirkwood, #Fanghorn, #West, #Rohan, #Dwarves-Iron-Hills, #Wraiths, #Orc-Mordor, #Dwarves-Blue-Mountains, #Elf-Lothlorien");
 
 function init() {
     for (let i = 0; i < table_row.length; i++) {
         table_row[i].addEventListener('click', fillEllipse, false);
-        table_row[i].addEventListener('click', fillPath, false);
+        /*table_row[i].addEventListener('click', fillPath, false);*/
     }
 }
 
 function fillEllipse() {
-    let clickClasses = this.class
+    let clickClasses = this.table_row.class
+    const subClass = clickClasses.split('-')[0];
+
+    document.subClass.
     /* ebb: You now need to 1) split() the class on spaces (look up how), and store the result in a new variable.
     /* This new variable stores a sequence of values.
     * THEN, loop through the new variable's sequence of values.
@@ -20,13 +23,9 @@ function fillEllipse() {
     after you cut off the # (probably with split() function again).
     In that step you also trigger the fill event!
      */
-     */
-     */
-     */
-    *  */
     for (let i = 0; i < ellipse.length; i++) {
-        if (ellipse[i].style.fill == "none"){
-            ellipse[i].style.fill = 0x002dff
+        if (ellipse[i].fillStyle == "none"){
+            ellipse[i].fillStyle = "blue"
         } else {
             ellipse[i].style.fill = "none"
         }
